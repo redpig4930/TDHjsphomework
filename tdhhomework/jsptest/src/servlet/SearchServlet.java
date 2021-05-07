9ba41d8b616ac269d07fbfa25e476cf714e38278
redpig4930 <83286166+redpig4930@users.noreply.github.com>
2021-05-07 10:14:10 +0800

    Update SearchServlet.java

--- tdhhomework/jsptest/src/servlet/SearchServlet.java
+++ tdhhomework/jsptest/src/servlet/SearchServlet.java
@@ -10,15 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-/**
- * @Author huzhenyu
- * @Description 查询用户servlet
- * @Project Name:jsptest
- * @File_Name: SearchServlet
- * @Package_Name:  servlet
- * @修改记录：
- *      2021.4.30   当id为空时，设置id为“_”，因为“_”为通配符，所以这样id为空时就会把所有记录查询出来。
- */
+
 public class SearchServlet extends HttpServlet {
     /**
      * 调用doGET，转到doPost
